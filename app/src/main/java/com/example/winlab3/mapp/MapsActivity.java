6ca802658a7af3e41ac027b05b60ef60a2e26014
gislebjoru <gisle_fb@yahoo.no>
2017-05-30 18:41:58 +0200

    wedone my dude2

--- app/src/main/java/com/example/winlab3/mapp/MapsActivity.java
+++ app/src/main/java/com/example/winlab3/mapp/MapsActivity.java
@@ -1,12 +1,9 @@
 package com.example.winlab3.mapp;
 
 import android.graphics.Color;
-import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
-import android.provider.Telephony;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +34,7 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    //definere variabler
     private String myFra;
     private String myTil;
     private GoogleMap mMap;
@@ -49,12 +47,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //onCreate hentin layout og mapFragment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_map);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //definere geocoder og finne knapp, edittext og textview
         final Geocoder geocoder = new Geocoder(this);
         Button btn = (Button) findViewById(R.id.searchbutton);
         final EditText editFra = (EditText) findViewById(R.id.fraText);
@@ -62,25 +62,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final TextView myText = (TextView) findViewById(R.id.wedidit);
 
 
+        //når knapp trykkes
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 if(myFra !="" && myTil !=""){
 
+                    //set myFra og myTil til inhold i editTexts
                     myFra = String.valueOf(editFra.getText());
                     myTil = String.valueOf(editTil.getText());
                         try {
+                            //hent latitude og longitude fra geocoder med innhold hentet fra myFra og myTil
                             fraLat = geocoder.getFromLocationName(myFra, 1).get(0).getLatitude();
                             fraLong = geocoder.getFromLocationName(myFra, 1).get(0).getLongitude();
                             tilLat = geocoder.getFromLocationName(myTil, 1).get(0).getLatitude();
                             tilLong = geocoder.getFromLocationName(myTil, 1).get(0).getLongitude();
+                            //lager LatLng som inneholder lat og lon fra myFra og myTil
                             LatLng fra = new LatLng(fraLat, fraLong);
                             LatLng til = new LatLng(tilLat, tilLong);
+                            //fjerner alle markers fra map(kan derfor søke flere ganger uten å ende opp med fler enn 2 markers
                             mMap.clear();
+                            //legg til markers på start og sluttpunkt
                             mMap.addMarker(new MarkerOptions().position(fra).title("Start"));
+                            mMap.addMarker(new MarkerOptions().position(til).title("Slutt"));
+                            //
                             mMap.moveCamera(CameraUpdateFactory.newLatLng(fra));
                             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(fra,15));
-                            mMap.addMarker(new MarkerOptions().position(til).title("Slutt"));
 
+                            //bruker GoogleDirection med serverkey, definerer innhold og hva vi ønsker å gjøre
                             GoogleDirection.withServerKey("AIzaSyDgMuCPcbDVRdkvY6_W9VNkij4RDRZPYOE")
                                     .from(new LatLng(fraLat, fraLong))
                                     .to(new LatLng(tilLat, tilLong))
@@ -93,48 +101,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                         @Override
                                         public void onDirectionSuccess(Direction direction, String rawBody) {
                                             if(direction.isOK()) {
+                                                //om directionOK henter den info
                                                 Route route = direction.getRouteList().get(0);
                                                 Leg leg = route.getLegList().get(0);
                                                 String info = String.valueOf(leg.getDistance().getText());
                                                 Info durationInfo = leg.getDuration();
                                                 String myDur = durationInfo.getText();
+                                                //setter textviews til variablen info og myDur
                                                 myText.setText("Distanse: "+info+"\n"+"Tid: "+myDur);
+                                                //tegner linje fra start til slutt
                                                 List<Step> stepList = direction.getRouteList().get(0).getLegList().get(0).getStepList();
                                                 ArrayList<PolylineOptions> polylineOptionList = DirectionConverter.createTransitPolyline(getApplicationContext(), stepList, 5, Color.RED, 3, Color.BLUE);
                                                 for (PolylineOptions polylineOption : polylineOptionList) {
                                                     mMap.addPolyline(polylineOption);
                                                 }
                                             } else {
+                                                //om direction ikke er ok
                                                 System.out.println("Direction not ok");
                                             }
                                         }
                                         @Override
                                         public void onDirectionFailure(Throwable t) {
+                                            //om direction feiler
                                             System.out.println("Directionfailure");
                                         }
                                     });
-
+                            //catch exceptions
                         } catch (IOException ex) {
                             System.out.println(ex);
                         } catch (IndexOutOfBoundsException e) {
                             System.out.println(e);
                         }
-
                     } else {
+                    //søkefeltene er tomme
                         System.out.println("sokefelt er tomme");
                     }
                 }
         });
     }
 
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //når map er ferdig lastet inn
         mMap = googleMap;
-
-
-
-
     }
 }
